859908730d1193b5736bec358fd3517405ff73ab
𝓐𝓷𝓪 <analygiaptc@gmail.com>
2025-08-28 15:01:03 -0300

    Add files via upload

---
+++ Aula Inicialização Repetições/script.js
@@ -0,0 +1,16 @@
+let x = 10;
+console.log("While");
+while(x >= 1){
+    console.log(x);
+    x--;
+}
+console.log("Do-While");
+x = 10;
+do{
+    console.log(x);
+    x--;
+}while(x >= 1);
+console.log("For");
+for(x = 10; x >= 1; x--){
+    console.log(x);
+}
