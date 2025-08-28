42435767118fd82ec990bbfc426e1ce51ddf2f43
𝓐𝓷𝓪 <analygiaptc@gmail.com>
2025-08-28 15:33:55 -0300

    Add files via upload

---
+++ Atividades Repetições/questao1.js
@@ -0,0 +1,10 @@
+let salario, soma;
+
+soma = 0;
+salario = parseFloat(prompt("Digite o salário (0 para encerrar):"));
+while(salario !== 0.0){
+    soma += salario;
+    salario = parseFloat(prompt("Digite o salário (0 para encerrar):"));
+}
+console.log("Total da família:", soma);
+
